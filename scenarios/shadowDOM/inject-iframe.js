5d54f13bb78fc51bf3950f13f7c002bab38c2002
Yash Vekaria <yvekaria.09@gmail.com>
2023-12-27 17:47:39 -0500

    Added testing for shadow and dynamic iframes

--- scenarios/shadowDOM/inject-iframe.js
+++ scenarios/shadowDOM/inject-iframe.js
@@ -1,43 +1,23 @@
-// Injecting iframe inside Iframe7
-const host7 = document.querySelector("#iframe7");
-const shadow1 = host7.attachShadow({ mode: "open" });
-const iframe10 = document.createElement("iframe");
-iframe10.src = "https://godaddy.com/";
-iframe10.title = "IFRAME10";
-const div10 = document.createElement("div");
-div10.textContent = "This is the tenth iframe (inside seventh iframe)";
-iframe10.append(div10);
-shadow.appendChild(iframe10);
+document.addEventListener("DOMContentLoaded", function() {
+    // Create the outer iframe
+    const outerIframe = document.createElement("iframe");
+    outerIframe.src = "outer-frame.html";
+    outerIframe.width = "600";
+    outerIframe.height = "400";
+    outerIframe.title = "Another Outer Frame";
 
-const shadow2 = host7.attachShadow({ mode: "closed" });
-const iframe11 = document.createElement("iframe");
-iframe11.src = "https://newyorker.com/";
-iframe11.title = "IFRAME11";
-const div11 = document.createElement("div");
-div11.textContent = "This is the eleventh iframe (inside seventh iframe)";
-iframe11.append(div11);
-shadow.appendChild(iframe11);
+    // Append the outer iframe to the body
+    document.body.appendChild(outerIframe);
 
+    // Create the inner iframe
+    const innerIframe = document.createElement("iframe");
+    innerIframe.src = "inner-frame.html";
+    innerIframe.width = "400";
+    innerIframe.height = "200";
+    innerIframe.title = "Shadow Inner Frame";
 
-// Injecting iframe inline with the body
-const host = document.querySelector("body");
-const shadow = host.attachShadow();
-const iframe12 = document.createElement("iframe");
-iframe12.src = "https://fern.com/";
-iframe12.title = "IFRAME12";
-const div10 = document.createElement("div");
-div12.textContent = "This is the twelfth iframe (inside body)";
-iframe12.append(div12);
-shadow.appendChild(iframe12);
-
-
-// Injecting iframe inside Iframe4
-const host4 = document.querySelector("#iframe4");
-const shadow3 = host4.attachShadow({ mode: "open" });
-const iframe13 = document.createElement("iframe");
-iframe13.src = "https://hbomax.com/";
-iframe13.title = "IFRAME13";
-const div13 = document.createElement("div");
-div13.textContent = "This is the thirteenth iframe (inside fourth iframe)";
-iframe13.append(div13);
-shadow3.appendChild(iframe13);
+    // Append the inner iframe to the outer iframe
+    // outerIframe.contentDocument.body.appendChild(innerIframe);
+    const shadow = outerIframe.attachShadow();
+    shadow.appendChild(innerIframe);
+});
